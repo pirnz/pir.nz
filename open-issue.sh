00f181abe41288bae9d2f50a01f75f796ac5b281
pirnz <pedro.iranzo@pm.me>
2022-01-08 18:38:05 +0100

    open issue with github cli

--- open-issue.sh
+++ open-issue.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 echo $NAME $EMAIL $REASON
-curl -x POST -u "pirnz":"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" https://api.github.com/repos/pirnz/pir.nz/issues -d '{"title":"Access request for $NAME","body":"Email: $EMAIL <br> $REASON"}'
+gh issue create --title "Access request for $NAME" --body "Email: $EMAIL <br>$REASON" --assignee @me
