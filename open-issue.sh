03647c6e95a55476ba03a5e629ce6ae48993aa59
pirnz <pedro.iranzo@pm.me>
2022-01-08 18:41:54 +0100

    add repo to create issue

--- open-issue.sh
+++ open-issue.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 echo $NAME $EMAIL $REASON
-gh issue create --title "Access request for $NAME" --body "Email: $EMAIL <br>$REASON" --assignee @me
+gh issue create --repo pirnz/pir.nz --title "Access request for $NAME" --body "Email: $EMAIL <br>$REASON" --assignee @me
