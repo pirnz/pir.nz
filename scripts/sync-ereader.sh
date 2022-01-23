2f520447af9446538fc8b68e0623d9a035cf9220
kepair <me@kepair.eu>
2022-01-23 22:09:25 +0100

    added notes

--- scripts/sync-ereader.sh
+++ scripts/sync-ereader.sh
@@ -4,3 +4,6 @@ root_path="/mnt/hdd/data/"
 echo "Syncing ereader content at $IP"
 sshpass -f $root_path/pir.nz/scripts/bookpass rsync --rsync-path=/mnt/secure/bin/rsync --progress --remove-source-files --update --verbose root@$IP:/mnt/ext1/Notes/* $root_path/pir.nz/static/thinks/books
 sshpass -f $root_path/pir.nz/scripts/bookpass rsync --rsync-path=/mnt/secure/bin/rsync --progress --update --verbose $root_path/books/* root@$IP:/mnt/ext1/Libros
+git add *.html
+git commit -m "book notes added"
+git push
