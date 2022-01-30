49210cde9619ad2aeb9a1faae10d57a70f082204
kepair <me@kepair.eu>
2022-01-30 12:06:30 +0100

    moved webhook out

--- scripts/open-issue.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-echo $NAME $EMAIL $REASON
-gh issue create --repo pirnz/pir.nz --title "Access request for $NAME" --body "Email: $EMAIL <br>$REASON" --assignee @me
--- scripts/sync-ereader.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-IP="192.168.1.$HOST"
-root_path="/mnt/hdd/data/"
-echo "Syncing ereader content at $IP"
-sshpass -f $root_path/pir.nz/scripts/bookpass rsync --rsync-path=/mnt/secure/bin/rsync --progress --remove-source-files --update --verbose root@$IP:/mnt/ext1/Notes/* $root_path/pir.nz/static/thinks/books
-sshpass -f $root_path/pir.nz/scripts/bookpass rsync --rsync-path=/mnt/secure/bin/rsync --progress --update --verbose $root_path/books/* root@$IP:/mnt/ext1/Libros
-git add **.html
-git commit -m "book notes added"
-git push
